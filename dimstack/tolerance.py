a5acbe79bfff79cd2a404ded95f5fe91f8b81bfe
phcreery <phcreery@gmail.com>
2023-02-27 22:32:11 -0600

    checkpoint

--- dimstack/tolerance.py
+++ dimstack/tolerance.py
@@ -2,6 +2,7 @@ from typing import List, Union
 import numpy as np
 import itertools
 import pandas as pd
+import matplotlib.pyplot as plt
 
 POSITIVE = "+"
 NEGATIVE = "-"
@@ -13,35 +14,74 @@ DIST_NORMAL_LT = "Normal LT"  # Normal distribution which has been screened in o
 DIST_NORMAL_GT = "Normal GT"  # Normal distribution which has been screened in order to remove lengths below a limit.
 
 
-def C_p(sigma: int, UL: int, LL: int) -> float:
-    return (UL - LL) / (6 * sigma)
-
-
-def C_pk(C_p: float, k: float) -> float:
-    return (1 - k) * C_p
-
-
-def sigma_i(T_i: int, sigma: int) -> float:
-    return T_i / sigma
-
-
-def standard_deviation(sigma_i: float, n: int) -> float:
-    return sigma_i / n**0.5
-
-
-def RSS(*args):
-    return (sum([arg**2 for arg in args])) ** 0.5
-
-
 def round(x, n=DECIMALS):
     return np.around(x, decimals=n)
 
 
+def sign(x):
+    """Return the sign of x, i.e. -1, 0 or 1.
+
+    >>> sign(0)
+    0
+    >>> sign(10)
+    1
+    >>> sign(-10)
+    -1
+    """
+    return (x > 0) - (x < 0)
+
+
+def C_p(UL: float, LL: float, sigma: float) -> float:
+    """Process capability index.
+
+    Args:
+        UL (int): _description_
+        LL (int): _description_
+        sigma (int): _description_
+
+    Returns:
+        float: _description_
+    """
+    return (UL - LL) / (6 * sigma)
+
+
+def C_pk(C_p: float, k: float) -> float:
+    """Process capability index. adjusted for centering.
+    Cpl = (mu - L)/3*sigma
+    Cpu = (U - mu)/3*sigma
+    C_pk = min(Cpl, Cpu) = (1 - k) * C_p
+
+    Args:
+        C_p (float): Process capability index.
+        k (float): ratio of the amount the center of the distribution is shifted from the nominal value to the standard deviation.
+
+    Returns:
+        float: Process capability index.
+    """
+    return (1 - k) * C_p
+
+
+def sigma_i(T_i: float, sigma: float) -> float:
+    return T_i / sigma
+
+
+def standard_deviation(sigma_i: float, n: float) -> float:
+    return sigma_i / n ** 0.5
+
+
+def RSS(*args):
+    return (sum([arg ** 2 for arg in args])) ** 0.5
+
+
 def C_f(t_rss, t_wc, n):
-    return ((0.5 * (t_wc - t_rss)) / (t_rss * (n**0.5 - 1))) + 1
+    return ((0.5 * (t_wc - t_rss)) / (t_rss * (n ** 0.5 - 1))) + 1
 
 
-class Bilateral:
+class SymmetricBilateral:
+    """Bilateral tolerancing is a method of specifying a tolerance that is symmetrical about the nominal value.
+    This is the most common type of tolerancing.
+    """
+
     def __init__(self, tol: float):
         self.tol = tol
 
@@ -61,13 +101,17 @@ class Bilateral:
         return 2 * self.tol
 
 
-class Unilateral:
+class UnequalBilateral:
+    """Bilateral tolerancing is a method of specifying a tolerance that is asymmetrical about the nominal value.
+    This can also be used for Unilateral tolerancing.
+    """
+
     def __init__(self, upper: float, lower: float):
         self.upper = upper
         self.lower = lower
 
     def __repr__(self) -> str:
-        return f"+{round(self.upper)}/-{round(self.lower)}"
+        return f"+ {round(self.upper)} / - {round(self.lower)}"
 
     @property
     def tol(self):
@@ -78,22 +122,38 @@ class Unilateral:
         return self.upper - self.lower
 
 
-class Measurement:
+class Dimension:
+    """
+    A measurement is a single measurement of a part.
+
+    Args:
+        nom (float, optional): The nominal value of the measurement. Defaults to 0.
+        tol (Union[SymmetricBilateral, UnequalBilateral], optional): The tolerance of the measurement. Defaults to SymmetricBilateral(0).
+        a (float, optional): The sensitivity of the measurement. Defaults to 1. If the nominal value is negative, the sensitivity will be multiplied by a -1 and the nominal value will be made positive.
+        process_sigma (float, optional): The standard deviation of the process. Defaults to 3.
+        k (float, optional): The ratio of the amount the center of the distribution is shifted from the nominal value to the standard deviation. Defaults to 0.
+        distribution (str, optional): The distribution of the measurement. Defaults to "Normal".
+        name (str, optional): The name of the measurement. Defaults to "Dimension".
+        desc (str, optional): The description of the measurement. Defaults to "Dimension".
+    """
+
     newID = itertools.count().__next__
 
     def __init__(
         self,
-        nom: int = 0,
-        tol: Union[Unilateral, Bilateral] = Bilateral(0),
-        process_sigma: int = 6,
-        k: int = 0,
+        nom: float = 0,
+        tol: Union[SymmetricBilateral, UnequalBilateral] = SymmetricBilateral(0),
+        a: float = 1,
+        process_sigma: float = 3,
+        k: float = 0,
         distribution: str = "Normal",
-        name: str = "Measurement",
-        desc: str = "Measurement",
+        name: str = "Dimension",
+        desc: str = "Dimension",
     ):
-        self.id = Measurement.newID()
-        self.nominal = nom
+        self.id = Dimension.newID()
+        self.nominal = abs(nom)
         self.tolerance = tol
+        self.a = a * sign(nom)  # sensitivity
         self.process_sigma = process_sigma
         self.k = k
         self.distribution = distribution
@@ -101,34 +161,14 @@ class Measurement:
         self.description = desc
 
     def __repr__(self) -> str:
-        return f"{self.id}: {self.direction}{round(self.distance)} {repr(self.tolerance)} {self.sigma}σ {self.name} {self.description}"
-
-    @property
-    def distance(self):
-        return np.abs(self.nominal)
+        return f"{self.id}: {self.direction}{round(self.nominal)} {repr(self.tolerance)} {self.sigma}σ {self.name} {self.description}"
 
     @property
     def direction(self):
-        if self.nominal >= 0:
+        if self.a >= 0:
             return POSITIVE
         else:
             return NEGATIVE
-
-    @property
-    def j(self):
-        """The variable name j represents the direction. j is either -1 or 1.
-
-        Returns:
-            int: direction multiplier
-        """
-        if self.direction == POSITIVE:
-            return 1
-        elif self.direction == NEGATIVE:
-            return -1
-
-    @property
-    def T(self):
-        return self.tolerance.T
 
     @property
     def upper_abs(self):
@@ -146,29 +186,27 @@ class Measurement:
 
     @property
     def min_rel(self):
-        return self.j * (self.distance - self.tolerance.lower)
+        return self.a * (self.nominal - self.tolerance.lower)
 
     @property
     def max_rel(self):
-        return self.j * (self.distance + self.tolerance.upper)
+        return self.a * (self.nominal + self.tolerance.upper)
 
     @property
     def min_abs(self):
-        # return self.distance - self.T / 2
-        return self.distance - self.tolerance.lower
+        return self.nominal - self.tolerance.lower
 
     @property
     def max_abs(self):
-        # return self.distance + self.T / 2
-        return self.distance + self.tolerance.upper
+        return self.nominal + self.tolerance.upper
 
     @property
     def sigma(self):
-        return abs(self.T / 2) / self.process_sigma
+        return abs(self.tolerance.T / 2) / self.process_sigma
 
     @property
     def C_p(self):
-        return C_p(self.sigma, self.max_abs, self.min_abs)
+        return C_p(self.max_abs, self.min_abs, self.sigma)
 
     @property
     def C_pk(self):
@@ -176,27 +214,37 @@ class Measurement:
 
     @property
     def mu_eff(self):
+        """effective mean"""
         return (self.max_abs + self.min_abs) / 2
 
     @property
     def sigma_eff(self):
-        return abs(self.T) / (6 * self.C_pk)
+        """effective standard deviation"""
+        return abs(self.tolerance.T) / (6 * self.C_pk)
 
 
 class Stack:
-    def __init__(self, title: str = "Stack", items: List[Measurement] = []):
+    def __init__(self, title: str = "Stack", items: List[Dimension] = []):
         self.title = title
         self.items = items
 
     def __repr__(self) -> str:
         return f"{self.title}"
 
-    def append(self, measurement: Measurement):
+    def append(self, measurement: Dimension):
         self.items.append(measurement)
 
     @property
-    def nom_stack(self):
-        return [item.nominal for item in self.items]
+    def nominal(self):
+        return sum([item.nominal * item.a for item in self.items])
+
+    @property
+    def mu(self):
+        return sum([item.mu_eff * item.a for item in self.items])
+
+    @property
+    def sigma(self):
+        return RSS(*[item.sigma_eff * item.a for item in self.items])
 
     @property
     def tol_stack_upper(self):
@@ -207,36 +255,12 @@ class Stack:
         return [item.lower_abs for item in self.items]
 
     @property
-    def d_g(self):
-        return sum(self.nom_stack)
+    def t_wc_upper(self) -> float:
+        return sum(filter(None, self.tol_stack_upper))
 
     @property
-    def t_wc_upper(self):
-        return sum(self.tol_stack_upper)
-
-    @property
-    def t_wc_lower(self):
-        return sum(self.tol_stack_lower)
-
-    @property
-    def t_rss_upper(self):
-        return RSS(*self.tol_stack_upper)
-
-    @property
-    def t_rss_lower(self):
-        return RSS(*self.tol_stack_lower)
-
-    @property
-    def t_mrss_lower(self):
-        return (
-            C_f(self.t_rss_lower, self.t_wc_lower, len(self.items)) * self.t_rss_lower
-        )
-
-    @property
-    def t_mrss_upper(self):
-        return (
-            C_f(self.t_rss_upper, self.t_wc_upper, len(self.items)) * self.t_rss_upper
-        )
+    def t_wc_lower(self) -> float:
+        return sum(filter(None, self.tol_stack_lower))
 
     def df(self):
         # members = [attr for attr in dir(obj()) if not callable(getattr(obj(),attr)) and not attr.startswith("__")]
@@ -245,12 +269,13 @@ class Stack:
                 {
                     "id": item.id,
                     "dir": item.direction,
-                    "nominal": np.abs(round(item.nominal)),
-                    "tolerance": (repr(item.tolerance)).ljust(len("tolerance"), " "),
+                    "nominal": round(item.nominal),
+                    "tolerance": (repr(item.tolerance)).ljust(14, " "),
                     "process_sigma": str(item.process_sigma) + "σ",
+                    "sensitivity": str(item.a),
                     "σ": round(item.sigma),
-                    "min_rel": round(item.min_rel),
-                    "max_rel": round(item.max_rel),
+                    # "min_rel": round(item.min_rel),
+                    # "max_rel": round(item.max_rel),
                     "min_abs": round(item.min_abs),
                     "max_abs": round(item.max_abs),
                     "C_p": round(item.C_p),
@@ -265,39 +290,127 @@ class Stack:
             ]
         )
 
-    def df_text(self):
-        return self.df().to_string(index=False)
+    def show_dimensions_text(self):
+        print(self.df().to_string(index=False))
 
-    def results_text_RSS_simple(self):
-        # Dimensioning and Tolerancing Handbook, McGraw Hill
-        # http://files.engineering.com/getfile.aspx?folder=69759f43-e81a-4801-9090-a0c95402bfc0&file=RSS_explanation.GIF
+    def show_results_text_WC(self):
+        """This is a simple Worst-Case calculation"""
+        s = "--[Worst Case]--------------------------------\n"
+        s += f"{round(self.nominal)} +{round(self.t_wc_upper)}/-{round(self.t_wc_lower)} [{round(self.nominal-self.t_wc_lower)} {round(self.nominal+self.t_wc_upper)}] Worst Case"
+        print(s)
+
+    def show_results_text_RSS_simple(self):
         """This is a simple RSS calculation. This is uses the RSS calculation method in the Dimensioning and Tolerancing Handbook, McGraw Hill.
         It is really only useful for a Bilateral stack of same process-sigma items. The RSS result has the same uncertainty as the measurements.
+        Historically, Eq. (9.11) assumed that all of the component tolerances (t_i) represent a 3si value for their
+        manufacturing processes. Thus, if all the component distributions are assumed to be normal, then the
+        probability that a dimension is between ±t_i is 99.73%. If this is true, then the assembly gap distribution is
+        normal and the probability that it is ±t_rss between is 99.73%.
+        Although most people have assumed a value of ±3s for piecepart tolerances, the RSS equation works
+        for “equal s” values. If the designer assumed that the input tolerances were ±4s values for the piecepart
+        manufacturing processes, then the probability that the assembly is between ±t_rss is 99.9937 (4s).
+        The 3s process limits using the RSS Model are similar to the Worst Case Model. The minimum gap is
+        equal to the mean value minus the RSS variation at the gap. The maximum gap is equal to the mean value
+        plus the RSS variation at the gap.
         """
-        s = "--[RSS simple]---------------------------------------\n"
-        s += f"{round(self.d_g)} +{round(self.t_wc_upper)}/-{round(self.t_wc_lower)} [{round(self.d_g-self.t_wc_lower)} {round(self.d_g+self.t_wc_upper)}] Worst Case \n"
-        s += f"{round(self.d_g)} +{round(self.t_mrss_upper)}/-{round(self.t_mrss_lower)} [{round(self.d_g-self.t_mrss_lower)} {round(self.d_g+self.t_mrss_upper)}] Modified RSS \n"
-        s += f"{round(self.d_g)} +{round(self.t_rss_upper)}/-{round(self.t_rss_lower)} [{round(self.d_g-self.t_rss_lower)} {round(self.d_g+self.t_rss_upper)}] RSS "
-        return s
+        # Dimensioning and Tolerancing Handbook, McGraw Hill
+        # http://files.engineering.com/getfile.aspx?folder=69759f43-e81a-4801-9090-a0c95402bfc0&file=RSS_explanation.GIF
 
-    def results_text_RSS(self):
+        # check if all items are the same process_sigma
+        # if len(set([item.process_sigma for item in self.items])) > 1:
+        #     raise ValueError(
+        #         "For simple RSS analysis, all items must have the same process_sigma"
+        #     )
+
+        # Convert all dimensions to mean dimensions with an equal bilateral tolerance
+        d_g = self.mu
+        t_wc = []
+        t_rss = []
+        for item in self.items:
+            a = item.a
+            t = item.tolerance.tol
+            t_wc.append(abs(a * t))
+            t_rss.append((a ** 2) * (t ** 2))
+
+        t_wc = sum(t_wc)
+        t_rss = RSS(*t_rss)
+        n = len(self.items)
+        C_f = (0.5 * (t_wc - t_rss)) / (t_rss * (np.sqrt(n) - 1)) + 1
+        t_mrss = C_f * t_rss
+
+        s = "--[RSS assuming uniform process variation]----\n"
+        s += f"{round(d_g)} ± {round(t_wc)} [{round(d_g-t_wc)} {round(d_g+t_wc)}] Worst Case \n"
+        s += f"{round(d_g)} ± {round(t_mrss)} [{round(d_g-t_mrss)} {round(d_g+t_mrss)}] Modified RSS \n"
+        s += (
+            f"{round(d_g)} ± {round(t_rss)} [{round(d_g-t_rss)} {round(d_g+t_rss)}] RSS"
+        )
+        print(s)
+
+    def show_results_text_RSS(self):
         # https://www.mitcalc.com/doc/tolanalysis1d/help/en/tolanalysis1d.htm
-        # mu = sum([item.nominal for item in self.items])
-        # sigma = RSS(*[sigma_i(item.T, item.process_sigma * 2) for item in self.items])
-        mu = sum([item.mu_eff * item.j for item in self.items])
-        sigma = RSS(*[item.sigma_eff * item.j for item in self.items])
-        s = "--[RSS]---------------------------------------\n"
-        s += f"μ = {round(mu)}\n"
+        # sigma = RSS(*[item.sigma_eff * item.a for item in self.items])
+        sigma = self.sigma
+        s = "--['6 Sigma']---------------------------------\n"
+        s += f"μ = {round(self.mu)}\n"
         s += f"σ = {round(sigma)}\n"
-        s += f"{round(mu)} ± {round(sigma*6)} [{round(mu-sigma*6)} {round(mu+sigma*6)}] 6σ \n"
-        # s += f"{round(mu)} ± {round(sigma*5)} [{round(mu-sigma*5)} {round(mu+sigma*5)}] 5sigma"
-        s += f"{round(mu)} ± {round(sigma*4.5)} [{round(mu-sigma*4.5)} {round(mu+sigma*4.5)}] 4.5σ \n"
-        s += f"{round(mu)} ± {round(sigma*4)} [{round(mu-sigma*4)} {round(mu+sigma*4)}] 4σ \n"
-        s += f"{round(mu)} ± {round(sigma*3)} [{round(mu-sigma*3)} {round(mu+sigma*3)}] 3σ \n"
-        s += f"{round(mu)} ± {round(sigma*2)} [{round(mu-sigma)} {round(mu+sigma*2)}] 2σ \n"
-        return s
+        for i in [6, 5, 4.5, 4, 3]:
+            s += f"{round(self.mu)} ± {round(sigma*i)} [{round(self.mu-sigma*i)} {round(self.mu+sigma*i)}] {i}σ \n"
+        print(s)
 
-    # def results_text_six_sigma(self):
+    # def show_results_text_six_sigma(self):
     #     # https://www.mitcalc.com/doc/tolanalysis1d/help/en/tolanalysis1d.htm
     #     s = "----------------------------------------\n"
     #     # s += f"{}\n"
+
+    def show_length_chart(self):
+        fig, axs = plt.subplots(1, 1, figsize=(6, 3), dpi=200)
+        axs.grid()
+        axs.set_axisbelow(True)
+        axs.axvline(0, label="DATUM", alpha=0.6)
+
+        # determine rough plot length to size the arrow heads
+        max = 0
+        min = 0
+        last_part_x = 0
+        for item in self.items:
+            last_part_x = last_part_x + item.nominal * item.a
+            if last_part_x > max:
+                max = last_part_x
+            if last_part_x < min:
+                min = last_part_x
+        head_width = (max + min) * 0.001
+        # head_width = 0.1
+
+        # draw arrows
+        num_of_parts = len(self.items)
+        last_part_x = 0
+        for i in range(num_of_parts):
+            item = self.items[i]
+            axs.arrow(
+                x=last_part_x,
+                dx=item.nominal * item.a,
+                y=i,
+                dy=0,
+                width=head_width / 5,
+                length_includes_head=True,
+                head_width=head_width,
+                head_length=head_width * 50,
+                color=(0, 0, 0),
+            )
+
+            last_part_x = last_part_x + item.nominal * item.a
+        axs.set_yticks(range(len(self.items)))
+        axs.set_yticklabels([item.name for item in self.items])
+        axs.invert_yaxis()
+        axs.set_title("Nominal Stackup Flow Chart")
+        axs.legend(bbox_to_anchor=(1.04, 1), loc="upper left")
+
+        fig.tight_layout()
+
+        # return fig
+
+
+if __name__ == "__main__":
+    import doctest
+
+    doctest.testmod()
--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import dimstack.tolerance
 import reference.stackups.stackups as stackups
 
-Unilateral = dimstack.tolerance.Unilateral
-Bilateral = dimstack.tolerance.Bilateral
+SymmetricBilateral = dimstack.tolerance.SymmetricBilateral
+UnequalBilateral = dimstack.tolerance.UnequalBilateral
 
 # dimstack.test()
 
@@ -15,42 +15,65 @@ stacks[1].append(-0.3190, 0.0050, "PN16", "Mounting face to rt. end")
 # print(stacks[1].stack[0]["j"])
 # print(f"RSS: {(0.0050**2 + 0.0100**2 + 0.0050**2)**0.5}")
 
-# m1 = dimstack.tolerance.Measurement(nom=5, tol=Bilateral(0.1), name="a", desc="a")
-# m2 = dimstack.tolerance.Measurement(nom=-10, tol=Bilateral(0.1), name="b", desc="b")
-# m3 = dimstack.tolerance.Measurement(nom=5, tol=Bilateral(0.1), name="c", desc="c")
-# m4 = dimstack.tolerance.Measurement(
-#     nom=1, tol=Unilateral(0.100, 0.0000), name="t", desc="t"
+# m1 = dimstack.tolerance.Dimension(
+#     nom=5, tol=SymmetricBilateral(0.1), name="a", desc="a"
+# )
+# m2 = dimstack.tolerance.Dimension(
+#     nom=-10, tol=SymmetricBilateral(0.1), name="b", desc="b"
+# )
+# m3 = dimstack.tolerance.Dimension(
+#     nom=5, tol=SymmetricBilateral(0.1), name="c", desc="c"
+# )
+# m4 = dimstack.tolerance.Dimension(
+#     nom=1, tol=UnequalBilateral(0.100, 0.0000), name="t", desc="t"
 # )
 # items = [m1, m2, m3, m4]
 
-m1 = dimstack.tolerance.Measurement(
-    nom=208, tol=Bilateral(0.036), process_sigma=6, k=0.25, name="a", desc="Shaft"
+m1 = dimstack.tolerance.Dimension(
+    nom=208,
+    tol=SymmetricBilateral(0.036),
+    process_sigma=6,
+    k=0.25,
+    name="a",
+    desc="Shaft",
 )
-m2 = dimstack.tolerance.Measurement(
-    nom=-1.75, tol=Unilateral(0, 0.06), process_sigma=3, name="b", desc="Retainer ring"
+m2 = dimstack.tolerance.Dimension(
+    nom=-1.75,
+    tol=UnequalBilateral(0, 0.06),
+    process_sigma=3,
+    name="b",
+    desc="Retainer ring",
 )
-m3 = dimstack.tolerance.Measurement(
-    nom=-23, tol=Unilateral(0, 0.12), process_sigma=3, name="c", desc="Bearing"
+m3 = dimstack.tolerance.Dimension(
+    nom=-23, tol=UnequalBilateral(0, 0.12), process_sigma=3, name="c", desc="Bearing"
 )
-m4 = dimstack.tolerance.Measurement(
-    nom=20, tol=Bilateral(0.026), process_sigma=3, name="d", desc="Bearing Sleeve"
+m4 = dimstack.tolerance.Dimension(
+    nom=20,
+    tol=SymmetricBilateral(0.026),
+    process_sigma=3,
+    name="d",
+    desc="Bearing Sleeve",
 )
-m5 = dimstack.tolerance.Measurement(
-    nom=-200, tol=Bilateral(0.145), process_sigma=3, name="e", desc="Case"
+m5 = dimstack.tolerance.Dimension(
+    nom=-200, tol=SymmetricBilateral(0.145), process_sigma=3, name="e", desc="Case"
 )
-m6 = dimstack.tolerance.Measurement(
-    nom=20, tol=Bilateral(0.026), process_sigma=3, name="f", desc="Bearing Sleeve"
+m6 = dimstack.tolerance.Dimension(
+    nom=20,
+    tol=SymmetricBilateral(0.026),
+    process_sigma=3,
+    name="f",
+    desc="Bearing Sleeve",
 )
-m7 = dimstack.tolerance.Measurement(
-    nom=-23, tol=Unilateral(0, 0.12), process_sigma=3, name="g", desc="Bearing"
+m7 = dimstack.tolerance.Dimension(
+    nom=-23, tol=UnequalBilateral(0, 0.12), process_sigma=3, name="g", desc="Bearing"
 )
 items = [m1, m2, m3, m4, m5, m6, m7]
+
 stack = dimstack.tolerance.Stack(
     title="Example stacks, i.e. an example for stackups.py", items=items
 )
-# stack.append(m1)
-# stack.append(m2)
-# stack.append(m3)
-print(stack.df_text())
-print(stack.results_text_RSS_simple())
-print(stack.results_text_RSS())
+
+stack.show_dimensions_text()
+stack.show_results_text_WC()
+stack.show_results_text_RSS_simple()
+stack.show_results_text_RSS()
