fix distribution scaling and make plotting more robust

src/dimstack/plot.py
@@ -1,27 +1,38 @@
+from typing import Union
+import itertools
+
+import numpy as np
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
 import plotly.express as px
-import numpy as np
-import itertools
-from .eval import Stack
+
+from .eval import Stack, BasicDimension, StatisticalDimension
 
 
-def plot_stack(stack: Stack):
-    colors = px.colors.qualitative.Plotly
-    col_pal_iterator = itertools.cycle(colors)
+class StackPlot:
+    def __init__(self):
+        colors = px.colors.qualitative.Plotly
+        self.col_pal_iterator = itertools.cycle(colors)
 
-    # fig = go.Figure()
-    fig = make_subplots(specs=[[{"secondary_y": True}]], x_title="Distance (mm)")
+        # fig = go.Figure()
+        fig = make_subplots(specs=[[{"secondary_y": True}]], x_title="Distance (mm)")
+        self.fig = fig
+        self.fig.update_yaxes(title_text="Dimension", secondary_y=False)
+        self.fig.update_yaxes(title_text="Probability Density", secondary_y=True)
+        self.fig.update_layout(go.Layout(title="Dimension chart"))
 
-    prev_pos = 0
-    for item in stack.items:
-        color = next(col_pal_iterator)
+    def show(self):
+        return self.fig.show()
+
+    def add_dimension(self, item: Union[BasicDimension, StatisticalDimension]):
+        prev_pos = 0
+        color = next(self.col_pal_iterator)
 
         new_pos = prev_pos + item.nominal * item.dir
         ll = min(new_pos + item.tolerance.upper * item.dir, new_pos - item.tolerance.lower * item.dir)
         ul = max(new_pos + item.tolerance.upper * item.dir, new_pos - item.tolerance.lower * item.dir)
 
-        fig.add_trace(
+        self.fig.add_trace(
             go.Scatter(
                 x=[prev_pos, new_pos],
                 y=[item.name, item.name],
@@ -35,7 +46,7 @@ def plot_stack(stack: Stack):
             )
         )
 
-        fig.add_trace(
+        self.fig.add_trace(
             go.Scatter(
                 x=[new_pos, ll],
                 y=[item.name, item.name],
@@ -47,7 +58,7 @@ def plot_stack(stack: Stack):
                 legendgroup=f"{item.name}",
             )
         )
-        fig.add_trace(
+        self.fig.add_trace(
             go.Scatter(
                 x=[new_pos, ul],
                 y=[item.name, item.name],
@@ -63,20 +74,117 @@ def plot_stack(stack: Stack):
         ul = max(item.nominal + item.tolerance.upper, item.nominal - item.tolerance.lower)
         ll = min(item.nominal + item.tolerance.upper, item.nominal - item.tolerance.lower)
         xrange = np.arange(ll, ul, 0.001)
-        fig.add_trace(
+        # plot the default distribution
+        self.fig.add_trace(
             go.Scatter(
                 line=dict(color=color, width=1),
                 name=f"{item.name} Distribution",
                 x=xrange * item.dir + prev_pos,
-                y=item.get_dist().pdf(xrange) * 0.001 * 100,
+                y=item.get_dist().pdf(xrange),
                 legendgroup=f"{item.name}",
             ),
             secondary_y=True,
         )
+        # plot any alternate distributions
+        for dist in item.get_alt_dists():
+            self.fig.add_trace(
+                go.Scatter(
+                    line=dict(color=color, width=1),
+                    name=f"{item.name} Distribution",
+                    x=xrange * item.dir + prev_pos,
+                    y=dist.pdf(xrange),
+                    legendgroup=f"{item.name}",
+                    opacity=0.5,
+                ),
+                secondary_y=True,
+            )
 
-        prev_pos = new_pos
+    def add_stack(self, stack: Stack):
+        prev_pos = 0
+        for item in stack.items:
+            color = next(self.col_pal_iterator)
 
-    fig.update_yaxes(title_text="Dimension", secondary_y=False)
-    fig.update_yaxes(title_text="Probability Density", secondary_y=True)
-    fig.update_layout(go.Layout(title=f"Dimension chart of: {stack.title}"))
-    fig.show()
+            new_pos = prev_pos + item.nominal * item.dir
+            ll = min(new_pos + item.tolerance.upper * item.dir, new_pos - item.tolerance.lower * item.dir)
+            ul = max(new_pos + item.tolerance.upper * item.dir, new_pos - item.tolerance.lower * item.dir)
+
+            self.fig.add_trace(
+                go.Scatter(
+                    x=[prev_pos, new_pos],
+                    y=[item.name, item.name],
+                    mode="lines+markers+text",
+                    line=dict(color=color, width=2),
+                    marker=dict(size=[0, 10]),
+                    name=item.name,
+                    text=["", repr(item)],
+                    textposition="bottom center",
+                    legendgroup=f"{item.name}",
+                )
+            )
+
+            self.fig.add_trace(
+                go.Scatter(
+                    x=[new_pos, ll],
+                    y=[item.name, item.name],
+                    mode="lines+markers+text",
+                    line=dict(color="black"),
+                    marker=dict(size=[0, 10], symbol="line-ns", line_width=2, line_color="black"),
+                    name=f"{item.name} Upper Tol",
+                    opacity=0.5,
+                    legendgroup=f"{item.name}",
+                )
+            )
+            self.fig.add_trace(
+                go.Scatter(
+                    x=[new_pos, ul],
+                    y=[item.name, item.name],
+                    mode="lines+markers+text",
+                    line=dict(color="black"),
+                    marker=dict(size=[0, 10], symbol="line-ns", line_width=2, line_color="black"),
+                    name=f"{item.name} Lower Tol",
+                    opacity=0.5,
+                    legendgroup=f"{item.name}",
+                )
+            )
+
+            ul = max(item.nominal + item.tolerance.upper, item.nominal - item.tolerance.lower)
+            ll = min(item.nominal + item.tolerance.upper, item.nominal - item.tolerance.lower)
+            xrange = np.arange(ll, ul, 0.001)
+            # plot the default distribution
+            self.fig.add_trace(
+                go.Scatter(
+                    line=dict(color=color, width=1),
+                    name=f"{item.name} Distribution",
+                    x=xrange * item.dir + prev_pos,
+                    y=item.get_dist().pdf(xrange),
+                    legendgroup=f"{item.name}",
+                ),
+                secondary_y=True,
+            )
+            # plot any alternate distributions
+            for dist in item.get_alt_dists():
+                self.fig.add_trace(
+                    go.Scatter(
+                        line=dict(color=color, width=1),
+                        name=f"{item.name} Distribution",
+                        x=xrange * item.dir + prev_pos,
+                        y=dist.pdf(xrange),
+                        legendgroup=f"{item.name}",
+                        opacity=0.5,
+                    ),
+                    secondary_y=True,
+                )
+
+            prev_pos = new_pos
+
+        return self
+
+    def add(self, item):
+        if isinstance(item, Stack):
+            self.add_stack(item)
+        elif isinstance(item, (BasicDimension, StatisticalDimension)):
+            self.add_dimension(item)
+        else:
+            raise TypeError(f"Cannot add {type(item)} to StackPlot")
+
+        return self